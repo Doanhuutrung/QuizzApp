b955c61798f3da8f7bc2e18b1261f32ac2ff901e
HuuTrung <huutrungdoan2202@gmail.com>
2023-06-09 05:12:00 +0700

    settup enviroment

---
+++ src/Component/Header/Header.js
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div>
+        <Link to="/">
+            Quizz Question by Trung
+        </Link>
+    </div>
+  )
+}
+
+export default Header;
